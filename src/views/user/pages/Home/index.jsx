892e3206d51c93457265c176295729d469701439
pedropalermodev <pedropalermo.dev@gmail.com>
2025-05-07 19:35:58 -0300

    Adição da barra de usuário página home

--- src/views/user/pages/Home/index.jsx
+++ src/views/user/pages/Home/index.jsx
@@ -1,13 +1,12 @@
 import { useContext, useState } from 'react';
 import { AuthContext } from '../../../../shared/contexts/AuthContext';
 import { useContent } from '../../../../shared/hooks/useContent';
+import { CiSearch } from "react-icons/ci";
 import './styles.scss';
 
 function Home() {
     const { userData, globalLoading } = useContext(AuthContext);
     const { contents } = useContent();
-
-    const [selectedContent, setSelectedContent] = useState(null);
 
     if (globalLoading) return <p>Loading...</p>;
     if (!userData) return <p>Dados do usuário não encontrados.</p>;
@@ -30,27 +29,22 @@ function Home() {
 
     return (
         <main className='home-app__container'>
-            <div className='home-app__welcome'>
-                <p className='home-app__welcome--name'>Olá, {primeiroNome}!</p>
-                <p className='home-app__welcome--text'>Veja o que preparamos para você hoje.</p>
+            <div className='home-app__content'>
+                <div className='home-app__welcome'>
+                    <p className='home-app__welcome--name'>Olá, {primeiroNome}!</p>
+                    <p className='home-app__welcome--text'>Veja o que preparamos para você hoje.</p>
+                </div>
+                <div className='home-app__search'>
+                    <input className='home-app__search--input' type="text" placeholder='Pesquisar...' />
+                    <button className='home-app__search--button'><CiSearch /></button>
+                </div>
             </div>
 
-            {selectedContent ? (
-                <div className="iframe-container">
-                    <button onClick={() => setSelectedContent(null)} className="btn-voltar">← Voltar</button>
-                    <h2>{selectedContent.titulo}</h2>
-                    <iframe
-                        className="content-iframe"
-                        src={selectedContent.arquivoUrl.replace("watch?v=", "embed/")}
-                        title={selectedContent.titulo}
-                        allowFullScreen
-                    />
-                </div>
-            ) : (
+            <div className='home-app__content'>
                 <section className="conteudos__categoria">
                     <div className="content__list">
                         {meditacaoManha.map(content => (
-                            <div key={content.id} className="content__card" onClick={() => setSelectedContent(content)}>
+                            <div key={content.id} className="content__card">
                                 <img
                                     src={getYouTubeThumbnail(content.arquivoUrl)}
                                     alt={`Capa de ${content.titulo}`}
@@ -58,13 +52,13 @@ function Home() {
                                 />
                                 <div className="content__info">
                                     <h3>{content.titulo}</h3>
-                                    <p>{content.descricao ?? ''}</p>
                                 </div>
                             </div>
                         ))}
                     </div>
                 </section>
-            )}
+            </div>
+
         </main>
     );
 }
